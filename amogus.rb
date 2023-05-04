a2255e0b2fb74ebccf39641b790ed6206658821b
negicy <45336807+negicy@users.noreply.github.com>
2023-05-04 15:30:54 +0900

    Create amogus.rb

---
+++ amogus.rb
@@ -0,0 +1,31 @@
+    
+               eval(%w(use_s ynth
+          :piano                )* "")
+        eval(%w(                 play_pa
+        ttern_                      timed(
+       [:C5,                         :Eb5,
+      :F5,:Fs5,:F5,:Eb5,               :C5], 
+     [               0.3,             0.3000
+    1,                0.3,             0.3,0.3,0.3, 
+     0               .30               ,0000]))*"")
+      eval(%w(sleep(0.                 700000000001
+       1                               100000000001
+       1                               100000000001
+       1                               100000000001
+       1                               100000000001
+       11                             1100000000001
+       111                           11111000000001
+       11111                      ))*"")# SUSSUSSUS
+       eval(%w(              play_pat t ern_timed([
+       :Bb4 ,:D5,:C5] ,[0.2,0.2000]))*"")
+       eval(%w(sleep(0.700000000000))*"")          
+       eval(%w(slee          p(0.7))*"")
+       eval(%w(play          _ pattern_ti
+       med([:C5,:Eb          5,:F5,:Fs5,:
+       F5,:Eb5,:Fs5          ],[0.3]))*"")    
+       eval(%w(slee          p(0. 7))*"")
+       eval(%w(play          _pattern_tim
+       ed([:Fs5,:F5,         :Eb 5,:Fs5,:
+       F5,:Eb5,:C5],         [ 0.2]))*"")
+         
+         
